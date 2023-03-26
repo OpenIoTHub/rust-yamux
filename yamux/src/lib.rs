4b6204ce4270b0cd3a6bfc8759c592b0414576c9
Yu Fang <yu@iotserv.com>
2023-03-26 23:30:03 +0800

    Adapt to embedded platforms

--- yamux/src/lib.rs
+++ yamux/src/lib.rs
@@ -39,9 +39,9 @@ pub use crate::frame::{
     FrameDecodeError,
 };
 #[cfg(not(target_os = "espidf"))]
-pub const DEFAULT_CREDIT: u32 = 4 * 1024; // as per yamux specification
-#[cfg(target_os = "espidf")]
 pub const DEFAULT_CREDIT: u32 = 256 * 1024; // as per yamux specification
+#[cfg(target_os = "espidf")]
+pub const DEFAULT_CREDIT: u32 = 4 * 1024; // as per yamux specification
 
 pub type Result<T> = std::result::Result<T, ConnectionError>;
 
@@ -66,9 +66,9 @@ const MAX_COMMAND_BACKLOG: usize = 32;
 /// For details on why this concrete value was chosen, see
 /// https://github.com/paritytech/yamux/issues/100.
 #[cfg(not(target_os = "espidf"))]
-const DEFAULT_SPLIT_SEND_SIZE: usize = 4 * 1024;
-#[cfg(target_os = "espidf")]
 const DEFAULT_SPLIT_SEND_SIZE: usize = 16 * 1024;
+#[cfg(target_os = "espidf")]
+const DEFAULT_SPLIT_SEND_SIZE: usize = 4 * 1024;
 
 /// Specifies when window update frames are sent.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -122,7 +122,7 @@ impl Default for Config {
     fn default() -> Self {
         Config {
             receive_window: DEFAULT_CREDIT,
-            max_buffer_size: 4 * 1024,
+            max_buffer_size: 1024 * 1024,
             max_num_streams: 8192,
             window_update_mode: WindowUpdateMode::OnRead,
             read_after_close: true,
@@ -133,7 +133,7 @@ impl Default for Config {
     fn default() -> Self {
         Config {
             receive_window: DEFAULT_CREDIT,
-            max_buffer_size: 1024 * 1024,
+            max_buffer_size: 4 * 1024,
             max_num_streams: 8192,
             window_update_mode: WindowUpdateMode::OnRead,
             read_after_close: true,
